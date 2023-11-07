890b0eb562681db5f9d3e703a85b2588fe94bd34
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-07 18:44:48 +0400

    Update Countries.tsx

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
 import { Country } from "./DataTypes";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 interface Props {
   darkMode: boolean;
   selectedCountries: Country[];
+  countries: Country[];
 }
 
 const Countries = ({ darkMode, selectedCountries }: Props) => {
@@ -16,42 +17,43 @@ const Countries = ({ darkMode, selectedCountries }: Props) => {
       {selectedCountries.map((country, index) => {
         return (
           <Fragment key={index}>
-            <Link to={`${country.name.common}`}>
+            {/* <Link> */}
+            <div
+              className={`md:w-[264px] hover:scale-110 duration-500 cursor-pointer ${
+                darkMode ? "bg-[#202C36]" : "bg-white"
+              } country-shadow`}
+            >
+              <img
+                src={country.flags.svg}
+                alt={`${country.name.common} image`}
+                className="w-full h-full md:max-w-[264px] md:max-h-[130px]"
+              />
               <div
-                className={`md:w-[264px] hover:scale-110 duration-500 cursor-pointer ${
-                  darkMode ? "bg-[#202C36]" : "bg-white"
-                } country-shadow`}
+                className={`pt-6 pl-6 pb-11 ${
+                  darkMode ? "text-white" : "#111517"
+                }`}
               >
-                <img
-                  src={country.flags.svg}
-                  alt={`${country.name.common} image`}
-                  className="w-full h-full md:max-w-[264px] md:max-h-[130px]"
-                />
-                <div
-                  className={`pt-6 pl-6 pb-11 ${
-                    darkMode ? "text-white" : "#111517"
-                  }`}
-                >
-                  <h1 className="text-lg font-extrabold leading-[26px] mb-4">
-                    {country.name.common}
-                  </h1>
-                  <div className="flex flex-col gap-2 text-sm font-semibold leading-4">
-                    <p>
-                      Population:{" "}
-                      <span className="font-light">{country.population}</span>
-                    </p>
-                    <p>
-                      Region:{" "}
-                      <span className="font-light">{country.region}</span>
-                    </p>
-                    <p>
-                      Capital:{" "}
-                      <span className="font-light">{country.capital}</span>
-                    </p>
-                  </div>
+                <h1 className="text-lg font-extrabold leading-[26px] mb-4">
+                  {country.name.common}
+                </h1>
+                <div className="flex flex-col gap-2 text-sm font-semibold leading-4">
+                  <p>
+                    Population:{" "}
+                    <span className="font-light">
+                      {country.population.toLocaleString()}
+                    </span>
+                  </p>
+                  <p>
+                    Region: <span className="font-light">{country.region}</span>
+                  </p>
+                  <p>
+                    Capital:{" "}
+                    <span className="font-light">{country.capital}</span>
+                  </p>
                 </div>
               </div>
-            </Link>
+            </div>
+            {/* </Link> */}
           </Fragment>
         );
       })}
