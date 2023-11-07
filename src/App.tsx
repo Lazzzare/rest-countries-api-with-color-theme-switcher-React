db801776eec849e64c9ef2846bdae3299b91b730
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-07 17:57:18 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import axios from "axios";
+import { Country } from "./components/DataTypes";
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Input from "./components/Input";
 import Filter from "./components/Filter";
 import Countries from "./components/Countries";
+import SingleCountry from "./components/SingleCountry";
+
+const baseURL = "https://restcountries.com/v3.1/all";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState<boolean>(false);
+  const [countries, setCountries] = useState<Country[] | null>(null);
+
+  useEffect(() => {
+    axios.get<Country[]>(baseURL).then((response) => {
+      setCountries(response.data);
+      console.log(response.data);
+    });
+  }, []);
+  if (!countries) return null;
+  const selectedCountries = countries.slice(0, 8);
   return (
     <div
-      className={`w-full min-h-screen ${
+      className={`w-full min-h-screen duration-700 ${
         darkMode ? "bg-[#202C36]" : "bg-[#FAFAFA]"
       }`}
     >
@@ -17,7 +33,21 @@ const App = () => {
         <Input darkMode={darkMode} />
         <Filter darkMode={darkMode} />
       </div>
-      <Countries darkMode={darkMode} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Countries
+              darkMode={darkMode}
+              selectedCountries={selectedCountries}
+            />
+          }
+        ></Route>
+        <Route
+          path={`/${countries[0].name.common}`}
+          element={<SingleCountry />}
+        ></Route>
+      </Routes>
     </div>
   );
 };
