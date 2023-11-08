e4b986fbe46b17edb3f2512632513115ea0b50ec
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-08 14:45:24 +0400

    add motions with framer :100:

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,6 +1,7 @@
 import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
 import { Fragment } from "react";
 import { Country } from "./DataTypes";
+import { motion } from "framer-motion";
 
 interface Props {
   darkMode: boolean;
@@ -22,33 +23,39 @@ const Filter = ({
 }: Props) => {
   return (
     <div className="z-50 lg:absolute right-6 lg:right-20">
-      <div
-        onClick={() => setFilterShow(!filterShow)}
-        className={`input-shadow flex flex-row justify-between w-[200px] items-center gap-2
-        ${darkMode ? "bg-[#2B3844]" : "bg-white"} rounded-[5px]
-      px-6 py-[14px] cursor-pointer`}
+      <motion.div
+        initial={{ opacity: 0, x: 300 }}
+        animate={{ opacity: 3, x: 0 }}
+        transition={{ duration: 1 }}
       >
-        <h3
-          className={`text-xs lg:text-sm leading-5 font-normal ${
-            darkMode ? "text-white" : "text-[#111517]"
-          }`}
+        <div
+          onClick={() => setFilterShow(!filterShow)}
+          className={`input-shadow flex flex-row justify-between w-[200px] items-center gap-2
+        ${darkMode ? "bg-[#2B3844]" : "bg-white"} rounded-[5px]
+        px-6 py-[14px] cursor-pointer`}
         >
-          Filter by Region
-        </h3>
-        {filterShow ? (
-          <AiOutlineArrowUp
-            className={`${
-              darkMode ? "text-white" : "text-black"
-            } w-[10px] h-[10px] lg:w-3 lg:h-3`}
-          />
-        ) : (
-          <AiOutlineArrowDown
-            className={`${
-              darkMode ? "text-white" : "text-black"
-            } w-[10px] h-[10px] lg:w-3 lg:h-3`}
-          />
-        )}
-      </div>
+          <h3
+            className={`text-xs lg:text-sm leading-5 font-normal ${
+              darkMode ? "text-white" : "text-[#111517]"
+            }`}
+          >
+            Filter by Region
+          </h3>
+          {filterShow ? (
+            <AiOutlineArrowUp
+              className={`${
+                darkMode ? "text-white" : "text-black"
+              } w-[10px] h-[10px] lg:w-3 lg:h-3`}
+            />
+          ) : (
+            <AiOutlineArrowDown
+              className={`${
+                darkMode ? "text-white" : "text-black"
+              } w-[10px] h-[10px] lg:w-3 lg:h-3`}
+            />
+          )}
+        </div>
+      </motion.div>
       {filterShow ? (
         <div
           className={`input-shadow px-6 py-[14px] lg:py-[18px] ${
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,52 @@
 import { BsFillMoonStarsFill, BsSun } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 interface Props {
   darkMode: boolean;
   setDarkMode: (e: boolean) => void;
+  setSelectedRegion: (e: string) => void;
 }
 
-const Header = ({ darkMode, setDarkMode }: Props) => {
+const Header = ({ darkMode, setDarkMode, setSelectedRegion }: Props) => {
   return (
-    <div
-      className={`header-shadow ${
-        darkMode ? "bg-[#2B3844] text-white" : "bg-white text-[#111517]"
-      } w-full py-[30px] px-4 lg:py-6 lg:px-20 flex justify-between items-center`}
+    <motion.div
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 3, y: 0 }}
+      transition={{ duration: 1.5 }}
     >
-      <h1 className="cursor-pointer text-sm lg:text-2xl font-extrabold leading-5">
-        Where in the world?
-      </h1>
       <div
-        onClick={() => setDarkMode(!darkMode)}
-        className="flex flex-row items-center gap-2 cursor-pointer"
+        className={`header-shadow ${
+          darkMode ? "bg-[#2B3844] text-white" : "bg-white text-[#111517]"
+        } w-full py-[30px] px-4 lg:py-6 lg:px-20 flex justify-between items-center`}
       >
-        {!darkMode ? (
-          <>
-            <BsFillMoonStarsFill className="w-4 h-4 lg:w-5 lg:h-5" />
-            <h2 className="text-xs lg:text-base font-semibold leading-normal">
-              Dark Mode
-            </h2>
-          </>
-        ) : (
-          <>
-            <BsSun className="w-4 h-4 lg:w-5 lg:h-5" />
-            <h2 className="text-xs lg:text-base font-semibold leading-normal">
-              Light Mode
-            </h2>
-          </>
-        )}
+        <h1
+          onClick={() => setSelectedRegion("All")}
+          className="cursor-pointer text-sm lg:text-2xl font-extrabold leading-5"
+        >
+          Where in the world?
+        </h1>
+        <div
+          onClick={() => setDarkMode(!darkMode)}
+          className="flex flex-row items-center gap-2 cursor-pointer"
+        >
+          {!darkMode ? (
+            <>
+              <BsFillMoonStarsFill className="w-4 h-4 lg:w-5 lg:h-5" />
+              <h2 className="text-xs lg:text-base font-semibold leading-normal">
+                Dark Mode
+              </h2>
+            </>
+          ) : (
+            <>
+              <BsSun className="w-4 h-4 lg:w-5 lg:h-5" />
+              <h2 className="text-xs lg:text-base font-semibold leading-normal">
+                Light Mode
+              </h2>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
