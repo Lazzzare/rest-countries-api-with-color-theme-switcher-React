b28972e5db55579b322dc44fd193c5d0c578c80b
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-13 19:34:27 +0400

    Update SingleCountry.tsx

--- src/components/SingleCountry.tsx
+++ src/components/SingleCountry.tsx
@@ -1,5 +1,37 @@
-const SingleCountry = () => {
-  return <div>SingleCountry</div>;
+// import { useEffect } from "react";
+import { Country } from "./DataTypes";
+import { useNavigate } from "react-router-dom";
+
+interface Props {
+  countries: Country[];
+  singleCountry: string | null;
+  setSingleCountry: (e: string) => void;
+}
+
+const SingleCountry = ({
+  countries,
+  singleCountry,
+  setSingleCountry,
+}: Props) => {
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate("/");
+    setSingleCountry("");
+  };
+  return (
+    <div>
+      <h1 onClick={handleGoBack}>Go Back</h1>
+      {countries.map((country) => {
+        return (
+          <div>
+            {country.name.common === singleCountry && (
+              <img src={country.flags.svg} />
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default SingleCountry;
